cd5618ede9ca54b13c947540c5489bc1eb7c41da
mrrazeghi <mammad.razeghi.@gmail.com>
2016-02-28 19:07:18 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -244,7 +244,7 @@ function create_config( )
       "italian_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {108862143},
     admin_users = {},
     disabled_channels = {}
   }
